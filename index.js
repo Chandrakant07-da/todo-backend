1e238459ce9b17ee299969d0a82a2f156a628123
Chandrakant <ckant912@gmail.com>
2022-05-11 16:57:23 +0530

    Commited

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
-// Import Express
-const express = require('express')
-const mongoose = require('mongoose')
-const apiroute = require('./routes/api_route')
-// const cors = require('cors')
+// Libraries
+// Import Express: 
+// => Initiates the server
+// => routing for API URL(GET,POST)
+// => Used middlewares cors and json data
+const express = require("express");
+// Import Mongoose: 
+// => Connect to the databse through url
+// => Develop Schema on what data types that needs to be stored in your database
+// => MongoDB functions like save, findOne, findALL => Queries
+const mongoose = require("mongoose");
+// Import Cors:
+// => For security reasons your browser block unnecesarilly
+// => Using cors to enable the origin => frontend to access
+// => the backend data
+const cors = require("cors");
 
-// Intialize the Application
-const app = express()
-
-// Setup Server Port
-const port = 8080
-app.use(express.json()) // Set Content Type to JSON
-
-// Send Message for your localhost
-app.use('/', apiroute)
-// app.use(cors())
- 
-
-
-const url = "mongodb+srv://chandra:Chandra123#@cluster0.xtbl1.mongodb.net/Kellton-Ecommerce?retryWrites=true&w=majority"
-
-mongoose.connect(url,{useNewUrlParser:true})
-.then(() => {
-    console.log('Database Connected')
-}).catch(err => console.log(err))
+// Custom Modules 
+const routes = require("./routes/routes");
 
 
+// Intiaited App Variable to express function 
+const app = express();
+
+// To use middlewares 
+// Middlewares: application not for real users on the 
+// JSON: The data that we are accepting through body is in JSON format
+// CORS: we are enabling cors for the entire application
+app.use(express.json(), cors());
+
+// From express itself we are using another middleware called Router
+app.use("/", routes);
 
 
-// Launch the Ecommerce Backend App
-app.listen(port, () => {
-    console.log(`Running the Todo App Backend on Port:  
-    "http://localhost:${port}/ `)
-})
+// Database Connection String
+const url =
+  "mongodb+srv://chandra:Chandra123@cluster0.xtbl1.mongodb.net/Kellton-Ecommerce?retryWrites=true&w=majority";
+
+// Used Mongoose.connect method to connect database
+// useNewUrlParser: true
+// currentURL: true // Deprecated 
+mongoose
+  .connect(url, { useNewUrlParser: true })
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((err) => console.log(err));
+
+// Creating a server at Port : 3000
+// From frontend the requests we are listening to it
+app.listen(3000, () => {
+  console.log("App listening on port 3000");
+});
--- models/TodoModel.js
+++ models/TodoModel.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 
+// Created a Schema
+// What type of data that it should take to mongodb
 const TodoSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+  },
   todoName: {
     type: String,
     required: true,
